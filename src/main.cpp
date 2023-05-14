2a8a05736db036d4131d8a04ad99bbb61256ccc9
Himanshu Goel <himanshu@hgoel.dev>
2023-05-13 23:38:04 -0400

    Add initial PCA9685 test code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <PiPCA9685/PCA9685.h>
+#include <errno.h>
 
-int main(int argc, char** argv)
+int main()
 {
-    printf("Hello World!\n");
-    return 0;
+   PiPCA9685::PCA9685 pca{};
+
+    pca.setPWMFreq(50);
+
+    for (int i = 0; i < 16; i++)
+    {
+        pca.setPWM(i, 0, 300);
+    }
 }
