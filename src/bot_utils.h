9af7b102704c552c473bdb9940bbec773cfa81d8
Himanshu Goel <himanshu@hgoel.dev>
2023-05-21 22:35:36 +0100

    Adjust servo angle ranges.

--- src/bot_utils.h
+++ src/bot_utils.h
@@ -47,24 +47,24 @@ static glm::vec3 calcLegPos(float a01, float a12, float a23, float a3e)
     
     glm::vec3 pos_2e = glm::vec3(0);
     glm::vec3 pos_2e_0 = glm::vec3(L23 * cosf(a23), L23 * sinf(a23), 0);   //Thigh bone
-    glm::vec3 pos_2e_1 = glm::vec3(-L3E * sinf(a3e), -L3E * cosf(a3e), 0);  //Lower leg bone
+    glm::vec3 pos_2e_1 = glm::vec3(-L3E * sinf(a3e + M_PI / 3.0f), -L3E * cosf(a3e + M_PI / 3.0f), 0);  //Lower leg bone
     glm::vec3 pos_2e_2 = glm::vec3(LET * cosf(-AET), LET * sinf(-AET), 0); //Foot
 
     pos_2e = pos_2e_0 + pos_2e_1 + pos_2e_2; //Leg position relative to hip
-    printf("pos_2e_0: %f %f %f\n", pos_2e_0.x, pos_2e_0.y, pos_2e_0.z);
-    printf("pos_2e_1: %f %f %f\n", pos_2e_1.x, pos_2e_1.y, pos_2e_1.z);
-    printf("pos_2e_2: %f %f %f\n", pos_2e_2.x, pos_2e_2.y, pos_2e_2.z);
-    printf("pos_2e: %f %f %f\n", pos_2e.x, pos_2e.y, pos_2e.z);
+    //printf("pos_2e_0: %f %f %f\n", pos_2e_0.x, pos_2e_0.y, pos_2e_0.z);
+    //printf("pos_2e_1: %f %f %f\n", pos_2e_1.x, pos_2e_1.y, pos_2e_1.z);
+    //printf("pos_2e_2: %f %f %f\n", pos_2e_2.x, pos_2e_2.y, pos_2e_2.z);
+    //printf("pos_2e: %f %f %f\n", pos_2e.x, pos_2e.y, pos_2e.z);
 
     //Rotate pos_2e around pos_ax by a12
     pos = glm::vec3(pos_2e.x * cosf(a12), pos_2e.y + L12 + L01, pos_2e.x * sinf(a12)); //Pelvis XZ rotation
-    printf("pos: %f %f %f\n", pos.x, pos.y, pos.z);
+    //printf("pos: %f %f %f\n", pos.x, pos.y, pos.z);
 
     //Rotate pos around origin by a01 along xy plane
     float pos_rellen = glm::length(glm::vec2(pos.x, pos.y));
-    printf ("pos_rellen: %f\n", pos_rellen);
+    //printf ("pos_rellen: %f\n", pos_rellen);
     pos = glm::vec3(pos_rellen * cosf(a01 - M_PI_4), pos_rellen * sinf(a01 - M_PI_4), pos.z); //Pelvis XY rotation
-    printf("pos: %f %f %f\n", pos.x, pos.y, pos.z);
+    //printf("pos: %f %f %f\n", pos.x, pos.y, pos.z);
 
     return pos;
 }
@@ -89,7 +89,7 @@ class ServoController{
 
         void Move(int servo_index, float angle)
         {
-            //printf("Servo %d: %d\n", servo_index, angle);
+            //printf("Servo %d: %f\n", servo_index, angle);
             //return;
             if (servo_index & 1) angle = M_PI - angle;
             int pwm = (SERVOMAX - SERVOMIN) * angle / M_PI + SERVOMIN;
